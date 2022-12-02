f1422ee84ece3a7161950f00e2705e50f8a9dc28
Michael Szerman <michaelszer@gmail.com>
2022-12-02 12:47:48 -0500

    project created

---
+++ lib/image_processor.ex
@@ -0,0 +1,18 @@
+defmodule ImageProcessor do
+  @moduledoc """
+  Documentation for `ImageProcessor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImageProcessor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
