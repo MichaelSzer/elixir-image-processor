c87a9cdd5e82b35e0f73f5c2ef422898df476aa4
MichaelSzer <michaelszer@gmail.com>
2022-12-04 11:38:02 -0500

    loaded image from path

--- lib/image_processor.ex
+++ lib/image_processor.ex
@@ -13,22 +13,33 @@ defmodule ImageProcessor do
 
   """
   def make_image_more_red() do
-    image_filename = "tea-coffee-cup.png"
-    image_filename
+    image_filename = "images/tea-coffee-cup.png"
+
+    case Path.relative(image_filename) |> Image.open() do
+      {:ok, image} ->
+        Image.avatar(image)
+
+      {:error, error_reason} ->
+        error_reason
+    end
   end
 
   def edit_image() do
     # Prompt edit options
 
-    IO.puts("Chose an edit option: \n 1. Make the whole image more red.\n 2. Make image black and white.\n")
+    IO.puts(
+      "Chose an edit option: \n 1. Make the whole image more red.\n 2. Make image black and white.\n"
+    )
 
     option = IO.gets("Option number? ") |> String.trim_trailing("\n") |> String.to_integer()
 
     case option do
       1 ->
-        "Image more red"
+        make_image_more_red()
+
       2 ->
         "Image black and white"
+
       _ ->
         IO.puts("Invalid option, please pick a select a number from the list. \n")
         edit_image()
