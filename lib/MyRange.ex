f2f4dee091f0ea34865b3b4e19f2a22c6a5d6f53
MichaelSzer <michaelszer@gmail.com>
2022-12-05 08:47:51 -0500

    Created module MyRange

---
+++ lib/MyRange.ex
@@ -0,0 +1,11 @@
+defmodule MyRange do
+  @enforce_keys [:first, :last, :step]
+  defstruct [:first, :last, :step]
+
+  @spec new(integer(), integer(), integer()) :: %MyRange{first: integer(), last: integer(), step: integer()}
+  def new(first, last, step \\ 1) do
+    %MyRange{first: first, last: last, step: step}
+  end
+end
+
+# Implementation of the Enum protocol.
