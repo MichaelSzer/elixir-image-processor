60a285fb4f304c9740e35eea22837b476851dae9
MichaelSzer <michaelszer@gmail.com>
2022-12-05 08:48:16 -0500

    Created module MessageStore

---
+++ lib/message_storage.ex
@@ -0,0 +1,17 @@
+defmodule MessageStore do
+
+  @spec start :: {:ok, pid()}
+  def start do
+    Task.start_link(fn -> wait_for_orders(%{}) end)
+  end
+
+  defp wait_for_orders(all_messages) do
+    receive do
+      {:get, person, sender} ->
+        send sender, Map.get(all_messages, person, [])
+        wait_for_orders(all_messages)
+      {:put, msg, person} ->
+        Map.put(all_messages, person, Map.get(all_messages, person, []) ++ [msg]) |> wait_for_orders()
+    end
+  end
+end
